fec0e6f0ec6aa31a3ea1183be3f0e9326a2f6c3b
sigis92 <hugringr@gmail.com>
2018-12-17 10:25:37 +0100

    file for functions

---
+++ chord_functions.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_sequences(train_x, train_y, seq_length):
+    """
+    Input:
+        train_x, train_y: Features and their corresponding labels
+        seq_length: the length of the sequences
+    Return:
+        lstm_x: a matrix where elements are on the form (sequence, sequence length, features)
+        lstm_y: a matrix where elements are on the form (sequence, classes)
+    Notes: 
+        Does not generate sequences for the last (seq_length - 1) datapoints
+        Takes quite a lot of computing with long sequence lengths.
+
+
+    """
+    #containers for the sequences we generate
+    lstm_input = []
+    lstm_output = []
+
+    for i in range(0, train_x.shape[0] - (seq_length +1)):
+        #Create a sequence
+        in_data = train_x.iloc[i:seq_length + i].values
+        lstm_input.append(in_data)
+
+        #Select the chord that would have appeared after the sequence
+        out_data = train_y.iloc[seq_length + i].values
+        lstm_output.append(out_data) 
+
+    #Convert to arrays
+    lstm_x = np.array(lstm_input)
+    lstm_y = np.array(lstm_output)
+
+    return lstm_x, lstm_y
+
